4087ec755e105b80f4c78b941e78a22a968f4212
Anuplab Chatterjee <anuplab.ck@gmail.com>
2023-04-23 20:51:44 +0530

    Post section design complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -124,7 +124,7 @@ export const Navbar = (props) => {
                                     <button type="button" className="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                                         <span className="visually-hidden">Toggle Dropdown</span>
                                     </button>
-                                    <ul className="dropdown-menu">
+                                    <ul className="dropdown-menu create-post-menu">
                                         <li onClick={(e)=>activatePopup(e)}>
                                             &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
